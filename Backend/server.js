01bc9bf937d5cb9262dcf8eb77ff19b2e7831c7c
Ari <arihantjain72000@gmail.com>
2024-08-01 17:27:33 +0530

    done

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 async function main() {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
